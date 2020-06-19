f0b96471c668b0ae53286fffca699ee5fc95ee52
hugotritsch <hugo.tritsch@epita.fr>
2020-06-19 16:53:31 +0200

    [App] add function browser and read_from_file

--- src/TextMiningApp.cpp
+++ src/TextMiningApp.cpp
@@ -1,9 +1,74 @@
+#include <iostream>
 #include <string>
 #include <unistd.h>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-int my_menu(){
+struct TrieNode{
+    char value;
+    int freq;
+    vector<std::shared_ptr<struct TrieNode>> childrens;
+    shared_ptr<struct TrieNode> parent;
+};
+
+vector<string> browse(string path, string word, shared_ptr<struct TrieNode> tree, int dist, int nb_error, int i, vector<string> words) {
+    if (nb_error > dist){
+        vector<string> empty(0);
+        return empty;
+    }
+    if (word[i] == '\0') {
+        words.push_back(path + '\0');
+        printf("Word : %s \n", path.c_str());
+        return words;
+    }
+    else {
+        for (unsigned j = 0; j < tree->childrens.size(); j++) {
+            string new_path = path;
+            new_path.push_back(tree->childrens[j]->value);
+            if (tree->childrens[j]->value == word[i]) {
+                words = browse(new_path, word, tree->childrens[j], dist, nb_error, i + 1, words);
+            } else {
+                words = browse(new_path, word, tree->childrens[j], dist, nb_error + 1, i + 1, words);
+            }
+        }
+        return words;
+    }
+}
+
+shared_ptr<struct TrieNode> read_from_file(ifstream& inFile) {
+    int size;
+    char value;
+    shared_ptr<struct TrieNode> root = make_shared<struct TrieNode>();
+    
+    inFile.read(reinterpret_cast<char*>(&root->value), sizeof(char));
+    inFile.read(reinterpret_cast<char *>(&root->freq), sizeof(int));
+    inFile.read(reinterpret_cast<char *>(&size), sizeof(int));
+    
+    for (int i = 0; i < size; i++) {
+        root->childrens.push_back(read_from_file(inFile));
+    }
+    return root;
+}
+
+void test_dist(shared_ptr<struct TrieNode> root, string word){
+    printf("\n%s\n", "début");
+    
+    printf("\n%s\n", "dist 0 :");
+    vector<string> words(1000);
+    browse("", word, root, 0, 0, 0, words);
+    printf("\n%s\n", "dist 1 :");
+    vector<string> words2(1000);
+    browse("", word, root, 1, 0, 0, words2);
+    printf("\n%s\n", "dist 2 :");
+    vector<string> words3(1000);
+    browse("", word, root, 2, 0, 0, words3);
+    
+    printf("\n%s\n", "fin");
+}
+
+int my_menu(shared_ptr<struct TrieNode> root){
     while (1) {
         char line[4096];
         if (isatty(0))
@@ -18,16 +83,25 @@ int my_menu(){
             char *approx = strtok(line, " \t");
             char *distance = strtok(NULL, " \t");
             char *word = strtok(NULL, " \t");
-            printf("%s", approx);
-            printf("%s", " ");
-            printf("%s", distance);
-            printf("%s", " ");
-            printf("%s", word);
-            printf("%s", "\n");
+
+            vector<string> words(1000); //PAS OUF
+            browse("", word, root, stoi(distance), 0, 0, words);
         }
     }
 }
 
-int main() {
-    my_menu();
+int main(int argc, char *argv[]) {
+    if (argc != 2)
+    {
+        printf("usage: %s FILE\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    ifstream inFile;
+    inFile.open(argv[1]);
+    if (!inFile){
+        cerr << "Unable to read the file";
+        exit(1);
+    }
+    shared_ptr<struct TrieNode> root = read_from_file(inFile);
+    my_menu(root);
 }
